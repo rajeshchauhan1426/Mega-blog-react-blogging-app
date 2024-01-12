b29cc13865633761782ec46e49839efc462b8f5e
rajeshchauhan1426 <rc810609@gmail>
2024-01-12 19:41:51 +0530

    css change

--- mega-blog/src/App.jsx
+++ mega-blog/src/App.jsx
@@ -1,38 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+
 import './App.css'
-import authService from "./appwrite/auth"
-import {login, logout} from "./store/authSlice"
-import { Footer, Header } from './components'
-import { Outlet } from 'react-router-dom'
 
-function App() {
-  const [loading, setLoading] = useState(true)
-  const dispatch = useDispatch()
 
-  useEffect(() => {
-    authService.getCurrentUser()
-    .then((userData) => {
-      if (userData) {
-        dispatch(login({userData}))
-      } else {
-        dispatch(logout())
-      }
-    })
-    .finally(() => setLoading(false))
-  }, [])
-  
-  return !loading ? (
-    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
-      <div className='w-full block'>
-        <Header />
-        <main>
-        TODO:  <Outlet />
-        </main>
-        <Footer />
-      </div>
-    </div>
-  ) : null
+function app(){
+  const[count, setCount] = useState(0)
+  return(
+    <>
+    <h1>A Blogging App in app write</h1>
+    </>
+  )
 }
-
-export default App
+export default app
